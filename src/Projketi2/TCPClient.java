b6b3aa0cd9618f433fcccfa096a27af8efee9ad7
gr3sa-h1 <154631164+gr3sa-h1@users.noreply.github.com>
2024-11-08 21:45:43 +0100

    Added TCPClient

--- src/Projketi2/TCPClient.java
+++ src/Projketi2/TCPClient.java
@@ -1,4 +1,20 @@
 package Projketi2;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class TCPClient {
+    private static final String SERVER_ADDRESS = "localhost"; 
+    private static final int PORT = 10000; 
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(SERVER_ADDRESS, PORT)) {
+
+            System.out.println("Connected to the server.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

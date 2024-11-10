ee10725e2cf8e3d6255d41eae3362092356c509a
Gresa-Hasani <gresahasani16@gmail.com>
2024-11-10 13:29:54 +0100

    "Client timeout & folder setup for TCP server"
    Co-Authored-By: Haki Pintolli <155005609+HakiPintolli@users.noreply.github.com>

--- src/Projketi2/TCPServer.java
+++ src/Projketi2/TCPServer.java
@@ -11,9 +11,16 @@ public class TCPServer {
 
     private static final int PORT = 10000;
     private static final int MAX_CLIENTS = 4;
+    private static final long TIMEOUT = 10*60*1000;
+    private static List<String> log = new ArrayList<>();
+    private static File serverFiles = new File("server_files");
     private static boolean maxClientsReached = false;
 
     public static void main(String[] args) {
+
+        if (!serverFiles.exists()) {
+            serverFiles.mkdir();
+        }
 
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server listening on port " + PORT);
@@ -36,5 +43,5 @@ public class TCPServer {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+    }
 }
